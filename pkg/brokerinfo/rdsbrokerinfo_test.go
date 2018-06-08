429e7b2286026b1ffe4bc8bf8ee9152007d24532
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2018-06-08 14:07:25 +0100

    Add pkg/brokerinfo AWS RDS implementation
    Add an implementation for brokerInfo that queries the AWS RDS api
    directly to retrieve the information, in a similar way than it is done
    in the RDS Broker[1]. We copy across to list instances and
    generate the password.
    
    We rely on the packages from RDS broker awsrds[2], which couples
    this code with the RDS broker itself. It also duplicates some logic
    in the password generation.
    
    We decided to go for this implementation as it was the quickest to
    implement without refactoring the RDS broker.
    
    Other approaches could be:
     - implement a secured endpoint in the RDS broker to retrieve the
       list of instances and the connection string.
     - refactor RDS broker to extract the shared logic in a module.
    
    [1] https://github.com/alphagov/paas-rds-broker/blob/eee2df8257264e9afdbe9bc1b942174882e5d0d5/rdsbroker/broker.go#L611-L616
    [2]  https://github.com/alphagov/paas-rds-broker/blob/eee2df8257264e9afdbe9bc1b942174882e5d0d5/rdsbroker/broker.go#L654-L670
    
    Signed-off-by: Hector Rivas Gandara <keymon@gmail.com>

---
+++ pkg/brokerinfo/rdsbrokerinfo_test.go
@@ -0,0 +1,98 @@
+package brokerinfo_test
+
+import (
+	"fmt"
+
+	"github.com/alphagov/paas-rds-broker/awsrds"
+	rdsfake "github.com/alphagov/paas-rds-broker/awsrds/fakes"
+
+	"github.com/alphagov/paas-rds-metric-collector/pkg/brokerinfo"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("RDSBrokerInfo", func() {
+	var (
+		brokerInfo     *brokerinfo.RDSBrokerInfo
+		fakeDBInstance *rdsfake.FakeDBInstance
+	)
+
+	BeforeEach(func() {
+		fakeDBInstance = &rdsfake.FakeDBInstance{}
+		brokerInfo = brokerinfo.NewRDSBrokerInfo(
+			"broker_name", "dbprefix", "12345", fakeDBInstance, logger)
+	})
+
+	Context("ListInstanceGUIDs()", func() {
+		BeforeEach(func() {
+			fakeDBInstance.DescribeByTagDBInstanceDetails = []*awsrds.DBInstanceDetails{
+				&awsrds.DBInstanceDetails{
+					Identifier:     "dbprefix-instance-id-1",
+					Address:        "endpoint-address-1.example.com",
+					Port:           5432,
+					DBName:         "dbprefix-db",
+					MasterUsername: "master-username",
+				},
+				&awsrds.DBInstanceDetails{
+					Identifier:     "dbprefix-instance-id-2",
+					Address:        "endpoint-address-2.example.com",
+					Port:           5432,
+					DBName:         "dbprefix-db",
+					MasterUsername: "master-username",
+				},
+			}
+		})
+
+		It("returns error if it fails retrieving existing instances in AWS", func() {
+			fakeDBInstance.DescribeByTagError = fmt.Errorf("Error calling rds.DescribeByTag(...)")
+
+			_, err := brokerInfo.ListInstanceGUIDs()
+			Expect(err).To(HaveOccurred())
+		})
+		It("lists the instances for the right tag", func() {
+			_, err := brokerInfo.ListInstanceGUIDs()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(fakeDBInstance.DescribeByTagKey).To(Equal("Broker Name"))
+			Expect(fakeDBInstance.DescribeByTagValue).To(Equal("broker_name"))
+		})
+		It("returns the list of instance GUIDs", func() {
+			instanceGUIDs, err := brokerInfo.ListInstanceGUIDs()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(instanceGUIDs).To(ConsistOf(
+				"instance-id-1",
+				"instance-id-2",
+			))
+		})
+	})
+
+	Context("ConnectionString()", func() {
+		BeforeEach(func() {
+			fakeDBInstance.DescribeDBInstanceDetails = awsrds.DBInstanceDetails{
+				Identifier:     "dbprefix-instance-id",
+				Address:        "endpoint-address.example.com",
+				Port:           5432,
+				DBName:         "dbprefix-db",
+				MasterUsername: "master-username",
+			}
+		})
+
+		It("returns error if it fails retrieving existing instances in AWS", func() {
+			fakeDBInstance.DescribeError = fmt.Errorf("Error calling rds.Describe(...)")
+
+			_, err := brokerInfo.ConnectionString("instance-id")
+			Expect(err).To(HaveOccurred())
+		})
+		It("retrieves information of the right AWS RDS instance", func() {
+			_, err := brokerInfo.ConnectionString("instance-id")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(fakeDBInstance.DescribeCalled).To(BeTrue())
+			Expect(fakeDBInstance.DescribeID).To(Equal("dbprefix-instance-id"))
+		})
+		It("returns the proper connection string", func() {
+			connectionString, err := brokerInfo.ConnectionString("instance-id")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(connectionString).To(Equal("postgresql://master-username:9Fs6CWnuwf0BAY3rDFAels3OXANSo0-M@endpoint-address.example.com:5432/dbprefix-db?sslmode=require"))
+		})
+	})
+})
