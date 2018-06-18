69160405348f58d059e7033e3243b9ea24b95d3f
Hector Rivas Gandara <keymon@gmail.com>
2018-06-18 15:09:02 +0100

    Ensure the service is stopped on signal
    When receiving a TERM or INTERRUP signal, we want to be
    sure that the process would first try to stop any running
    worker and then exit after a timeout.
    
    We do not want to block for ever so we add a timeout in the
    signal handler in main. Receiving other signal will stop the
    process immediatelly
    
    Signed-off-by: Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"time"
+
 	_ "github.com/lib/pq"
 
 	. "github.com/onsi/ginkgo"
@@ -15,6 +17,35 @@ var _ = Describe("utils", func() {
 
 		It("should fail to find a string in slice", func() {
 			Expect(SliceContainsString([]string{"a"}, "b")).To(BeFalse())
+		})
+	})
+	Context("WithTimeout", func() {
+		It("runs a function and return false if does not timeout", func() {
+			executed := false
+			startTime := time.Now()
+			timeout := 1 * time.Second
+			ret := WithTimeout(timeout, func() { executed = true })
+			endTime := time.Now()
+
+			Expect(startTime.Add(timeout)).To(BeTemporally(">", endTime))
+			Expect(executed).To(BeTrue())
+			Expect(ret).To(BeFalse())
+		})
+
+		It("runs a function and return true if does timeout", func() {
+			executed := false
+			startTime := time.Now()
+			timeout := 1 * time.Second
+
+			stopIt := make(chan bool, 1)
+			defer func() { stopIt <- true }()
+
+			ret := WithTimeout(timeout, func() { executed = true; <-stopIt })
+			endTime := time.Now()
+
+			Expect(startTime.Add(timeout)).To(BeTemporally("~", endTime, 100*time.Millisecond))
+			Expect(executed).To(BeTrue())
+			Expect(ret).To(BeTrue())
 		})
 	})
 })
