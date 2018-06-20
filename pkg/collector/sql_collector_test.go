79e2ae269d5f0867226d23d4a3f7e8556ced9c62
Hector Rivas Gandara <keymon@gmail.com>
2018-06-20 15:23:48 +0100

    sql_collector allows include tags in queries
    We want to add dynamic tags like database name, table name and so on
    to the metrics retrieved using SQL queries.
    
    For that we extend the sql connector, so that it will consider
    any additional column not defined as `MetricQueryMeta` as tags,
    and add it to the returned metrics.

--- pkg/collector/sql_collector_test.go
+++ pkg/collector/sql_collector_test.go
@@ -16,7 +16,14 @@ import (
 
 var testQueries = []MetricQuery{
 	{
-		Query: "SELECT 1::integer as foo, '2'::varchar as bar, 3::double precision as baz",
+		Query: `
+			SELECT
+				1::integer as foo,
+				'2'::varchar as bar,
+				3::double precision as baz,
+				'val1' as tag1,
+				'val2' as tag2
+		`,
 		Metrics: []MetricQueryMeta{
 			{Key: "foo", Unit: "b"},
 			{Key: "bar", Unit: "s"},
@@ -39,7 +46,10 @@ var testQueries = []MetricQuery{
 		Query: "SELECT * FROM hell",
 	},
 	{
-		Query: "SELECT 'Hello World'",
+		Query: "SELECT 'Hello World' as foo2",
+		Metrics: []MetricQueryMeta{
+			{Key: "foo2", Unit: "gauge"},
+		},
 	},
 }
 
@@ -155,12 +165,13 @@ var _ = Describe("sql_collector", func() {
 		It("can collect all metrics from multiple queries", func() {
 			collectedMetrics, err := collector.Collect()
 			Expect(err).NotTo(HaveOccurred())
-			expectedTags := map[string]string{"source": "sql"}
+			expectedTags1 := map[string]string{"source": "sql", "tag1": "val1", "tag2": "val2"}
+			expectedTags2 := map[string]string{"source": "sql"}
 			Expect(collectedMetrics).To(ConsistOf(
-				metrics.Metric{Key: "foo", Value: 1, Unit: "b", Tags: expectedTags},
-				metrics.Metric{Key: "bar", Value: 2, Unit: "s", Tags: expectedTags},
-				metrics.Metric{Key: "baz", Value: 3, Unit: "conn", Tags: expectedTags},
-				metrics.Metric{Key: "foo2", Value: 1, Unit: "gauge", Tags: expectedTags},
+				metrics.Metric{Key: "foo", Value: 1, Unit: "b", Tags: expectedTags1},
+				metrics.Metric{Key: "bar", Value: 2, Unit: "s", Tags: expectedTags1},
+				metrics.Metric{Key: "baz", Value: 3, Unit: "conn", Tags: expectedTags1},
+				metrics.Metric{Key: "foo2", Value: 1, Unit: "gauge", Tags: expectedTags2},
 			))
 		})
 
@@ -187,14 +198,14 @@ var _ = Describe("helpers", func() {
 		dbConn.Close()
 	})
 
-	Context("getRowDataAsMap()", func() {
+	Context("getRowDataAsMaps()", func() {
 		It("should error when unexpected type from database", func() {
 			rows, err := dbConn.Query("SELECT 'Hello World'")
 
 			Expect(err).NotTo(HaveOccurred())
 
 			for rows.Next() {
-				_, err = getRowDataAsMap(rows)
+				_, _, err = getRowDataAsMaps(1, rows)
 				Expect(err).To(HaveOccurred())
 				Expect(err).To(MatchError(MatchRegexp("converting driver.Value type .+")))
 			}
@@ -206,24 +217,91 @@ var _ = Describe("helpers", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			for rows.Next() {
-				rows.Close()
-				_, err = getRowDataAsMap(rows)
+				rows.Close() // Close the rows
+				_, _, err = getRowDataAsMaps(1, rows)
 				Expect(err).To(HaveOccurred())
 				Expect(err).To(MatchError(MatchRegexp("Rows are closed")))
 			}
 		})
 
-		It("should succeed when returning a values", func() {
-			rows, err := dbConn.Query("SELECT 1::integer as foo, '2'::varchar as bar, 3::double precision as baz")
+		It("should succeed when returning values", func() {
+			rows, err := dbConn.Query(`
+				SELECT
+					1::integer as foo,
+					'2'::varchar as bar,
+					3::double precision as baz
+			`)
 
 			Expect(err).NotTo(HaveOccurred())
 
 			for rows.Next() {
-				data, err := getRowDataAsMap(rows)
+				data, _, err := getRowDataAsMaps(3, rows)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(data).To(Equal(map[string]float64{"foo": 1.0, "bar": 2.0, "baz": 3.0}))
 			}
 		})
+
+		It("should returning only the number of values indicated", func() {
+			rows, err := dbConn.Query(`
+				SELECT
+					1::integer as foo,
+					'2'::varchar as bar,
+					3::double precision as baz
+			`)
+
+			Expect(err).NotTo(HaveOccurred())
+
+			for rows.Next() {
+				data, _, err := getRowDataAsMaps(1, rows)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(data).To(Equal(map[string]float64{"foo": 1.0}))
+			}
+		})
+
+		It("should fail if the query does not have enough values", func() {
+			rows, err := dbConn.Query("SELECT 1::integer as foo")
+
+			Expect(err).NotTo(HaveOccurred())
+
+			for rows.Next() {
+				_, _, err := getRowDataAsMaps(4, rows)
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(MatchError(MatchRegexp("Expected 4 values but the row only has 1 columns")))
+			}
+		})
+
+		It("should succeed when returning values and tags", func() {
+			rows, err := dbConn.Query(`
+				SELECT
+					1::integer as foo,
+					'2'::varchar as bar,
+					3::double precision as baz,
+					'val1' as tag1,
+					'val2' as tag2
+			`)
+
+			Expect(err).NotTo(HaveOccurred())
+
+			for rows.Next() {
+				data, tags, err := getRowDataAsMaps(3, rows)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(data).To(Equal(map[string]float64{"foo": 1.0, "bar": 2.0, "baz": 3.0}))
+				Expect(tags).To(Equal(map[string]string{"tag1": "val1", "tag2": "val2"}))
+			}
+		})
+		It("should succeed tags is not a string", func() {
+			rows, err := dbConn.Query("SELECT 1::integer as foo, 1 as tag1")
+
+			Expect(err).NotTo(HaveOccurred())
+
+			for rows.Next() {
+				data, tags, err := getRowDataAsMaps(1, rows)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(data).To(Equal(map[string]float64{"foo": 1.0}))
+				Expect(tags).To(Equal(map[string]string{"tag1": "1"}))
+			}
+		})
+
 	})
 
 	Context("queryToMetrics()", func() {
@@ -253,7 +331,7 @@ var _ = Describe("helpers", func() {
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(len(rowMetrics)).To(Equal(3))
-			expectedTags := map[string]string{"source": "sql"}
+			expectedTags := map[string]string{"source": "sql", "tag1": "val1", "tag2": "val2"}
 			Expect(rowMetrics).To(Equal([]metrics.Metric{
 				{Key: "foo", Value: 1, Unit: "b", Tags: expectedTags},
 				{Key: "bar", Value: 2, Unit: "s", Tags: expectedTags},
